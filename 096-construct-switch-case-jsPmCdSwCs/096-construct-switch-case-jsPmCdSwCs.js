ad9c5cd09ec670bff4b1ae0ae5409b3cf5f7723d
Maxim Mogilevskiy <mogilevskiymv@gmail.com>
2023-03-09 12:53:26 +0300

    096-construct-switch-case-jsPmCdSwCs

---
+++ 096-construct-switch-case-jsPmCdSwCs/096-construct-switch-case-jsPmCdSwCs.js
@@ -0,0 +1,32 @@
+console.log("title", "096-construct-switch-case-jsPmCdSwCs")
+
+/*   let lang = 'ru';
+  if (lang == 'ru') {
+    console.log('рус');
+  } else if (lang == 'en') {
+    console.log('анг');
+  } else if (lang == 'de') {
+    console.log('нем');
+  } else {
+    console.log('язык не поддерживается');
+  } */
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+let lang = "en"
+
+switch (lang) {
+  case "ru":
+    console.log("рус")
+    break;
+  case "en":
+    console.log("анг")
+    break;
+  case "de":
+    console.log("нем")
+    break;
+  default:
+    console.log('язык не поддерживается')
+    break;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
